51ce1c01b5a65a22c0a9d5f461fab6fc16da2181
Igor Karpenko <majjor25karpenko@yandex.ru>
2022-06-11 15:18:45 +0300

    return fix localhost

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -13,19 +13,19 @@ class Api {
     }
 
     getUserServerInfo() {
-        return fetch(`${this._baseUrl}/user/me`, {
+        return fetch(`${this._baseUrl}/users/me`, {
             headers: this._headers,
         }).then(this._errorHandler)
     }
 
     getCards() {
-        return fetch(`${this._baseUrl}/card`, {
+        return fetch(`${this._baseUrl}/cards`, {
             headers: this._headers
         }).then(this._errorHandler)
     }
 
     addCard(item) {
-        return fetch(`${this._baseUrl}/card`, {
+        return fetch(`${this._baseUrl}/cards`, {
             method: 'POST',
             headers: this._headers,
             body: JSON.stringify(item) 
@@ -33,7 +33,7 @@ class Api {
     }
     
     editProfile(data) {
-        return fetch(`${this._baseUrl}/user/me`, {
+        return fetch(`${this._baseUrl}/users/me`, {
             method: 'PATCH',
             headers: this._headers,
             body: JSON.stringify(data)
@@ -41,7 +41,7 @@ class Api {
     }
 
     editAvatar(data) {
-        return fetch(`${this._baseUrl}/user/me/avatar`, {
+        return fetch(`${this._baseUrl}/users/me/avatar`, {
             method: 'PATCH',
             headers: this._headers,
             body: JSON.stringify(data)
@@ -49,21 +49,21 @@ class Api {
     }
 
     deleteCard(cardId) {
-        return fetch(`${this._baseUrl}/card/${cardId}`, {
+        return fetch(`${this._baseUrl}/cards/${cardId}`, {
             method: 'DELETE',
             headers: this._headers
         }).then(this._errorHandler)
     }
 
     deleteLike(cardId) {
-        return fetch(`${this._baseUrl}/card/like/${cardId}`, {
+        return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
             method: 'DELETE',
             headers: this._headers
         }).then(this._errorHandler)
     }
 
     addLike(cardId, isLiked) {
-        return fetch(`${this._baseUrl}/card/${cardId}/like`, {
+        return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
             method: `${isLiked ? 'PUT' : 'DELETE'}`,
             headers: this._headers
         }).then(this._errorHandler)
@@ -74,7 +74,7 @@ class Api {
 
 const token = localStorage.getItem('jwt');
 export const api = new Api({
-    baseUrl: 'https://api.praktikum.karpenko.nomoredomains.xyz',
+    baseUrl: 'http://api.praktikum.karpenko.nomoredomains.xyz',
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
